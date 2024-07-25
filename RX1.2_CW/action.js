fe8a8c9ec391c25c4738482d7693e0ed4b2eb10b
7vTr0x7 <7vTrox7@gmail.com>
2024-07-25 19:11:12 +0530

    RX1.1_CW - action

--- RX1.2_CW/action.js
+++ RX1.2_CW/action.js
@@ -1,2 +1,12 @@
 export const TODO_ADD = "todo/added";
 export const TODO_REMOVE = "todo/removed";
+
+export const addTodo = (text) => ({
+  type: TODO_ADD,
+  payload: text,
+});
+
+export const removeTodo = (index) => ({
+  type: TODO_REMOVE,
+  payload: index,
+});
