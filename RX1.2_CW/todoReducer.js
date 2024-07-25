0d2786e12f76572b91ead6f8907829a62c3580fe
7vTr0x7 <7vTrox7@gmail.com>
2024-07-25 18:58:13 +0530

    RX1.1_CW - todoReducer

---
+++ RX1.2_CW/todoReducer.js
@@ -0,0 +1,18 @@
+const initialState = { todos: [] };
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "TODO_ADD":
+      return { ...state, todos: [...state.todos, action.payload] };
+    case "TODO_REMOVE":
+      return {
+        ...state,
+        todos: state.todos.filter((todo, index) => index != action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
