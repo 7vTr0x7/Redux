8df89019198a78e75fa4b0e9a0d5bd12ec62783e
7vTr0x7 <7vTrox7@gmail.com>
2024-07-26 16:56:41 +0530

    RX1_HW2 - action

--- RX1_HW2/profileReducer.js
+++ RX1_HW2/profileReducer.js
@@ -1,20 +1,22 @@
+import { ADD_PROFILE, CALCULATE_AVERAGE_AGE, REMOVE_PROFILE } from "./action";
+
 const initialState = { profiles: [], averageAge: 0 };
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_PROFILE":
+    case ADD_PROFILE:
       return {
         ...state,
         profiles: [...state.profiles, action.payload],
       };
-    case "REMOVE_PROFILE":
+    case REMOVE_PROFILE:
       return {
         ...state,
         profiles: state.profiles.filter(
           (profile) => profile.id != action.payload
         ),
       };
-    case "CALCULATE_AVERAGE_AGE":
+    case CALCULATE_AVERAGE_AGE:
       const totalAge = state.profiles.reduce((acc, curr) => acc + curr.age, 0);
       const avgAge = totalAge / state.profiles.length;
       return {
