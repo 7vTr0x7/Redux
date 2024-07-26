7321d069f1a1696f5fd0371cdd570f5d65bc1037
7vTr0x7 <7vTrox7@gmail.com>
2024-07-26 12:22:03 +0530

    RX1_HW1 - cartReducer

---
+++ RX1_HW1/cartReducer.js
@@ -0,0 +1,29 @@
+const initialState = { cartItems: [] };
+
+const cartReducer = (state = initialState, action) => {
+  const item = state.cartItems.find((item) => item.id == action.payload.id);
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cartItems: [
+          ...state.cartItems,
+          item
+            ? { ...item, quantity: item.quantity + 1 }
+            : { ...action.payload, quantity: 1 },
+        ],
+      };
+    case "REMOVE_FROM_CART":
+      return state.cartItems.filter((item) => item.id != action.payload);
+    case "CALCULATE_TOTAL":
+      return state.cartItems.reduce(
+        (acc, curr) => acc + curr.price * curr.quantity,
+        0
+      );
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
