af3729b7696997b4a76bcdbbf4bd5fee67749d39
7vTr0x7 <7vTrox7@gmail.com>
2024-07-26 14:56:11 +0530

    RX1_HW1 - changes

--- RX1_HW1/cartReducer.js
+++ RX1_HW1/cartReducer.js
@@ -1,26 +1,26 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, CALCULATE_TOTAL } from "./action.js";
-const initialState = { cartItems: [] };
+const initialState = { cartItems: [], totalPrice: 0 };
 
 const cartReducer = (state = initialState, action) => {
-  const item = state.cartItems.find((item) => item.id == action.payload.id);
   switch (action.type) {
     case ADD_TO_CART:
       return {
         ...state,
-        cartItems: [
-          ...state.cartItems,
-          item
-            ? { ...item, quantity: item.quantity + 1 }
-            : { ...action.payload, quantity: 1 },
-        ],
+        cartItems: [...state.cartItems, action.payload],
       };
     case REMOVE_FROM_CART:
-      return state.cartItems.filter((item) => item.id != action.payload);
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id != action.payload),
+      };
     case CALCULATE_TOTAL:
-      return state.cartItems.reduce(
-        (acc, curr) => acc + curr.price * curr.quantity,
-        0
-      );
+      return {
+        ...state,
+        totalPrice: state.cartItems.reduce(
+          (acc, curr) => acc + curr.price * curr.quantity,
+          0
+        ),
+      };
 
     default:
       return state;
