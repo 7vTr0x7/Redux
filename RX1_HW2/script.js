00c9c82482884ce3fd40964876793b2bc6378443
7vTr0x7 <7vTrox7@gmail.com>
2024-07-26 17:03:13 +0530

    RX1_HW2 - store

--- RX1_HW2/script.js
+++ RX1_HW2/script.js
@@ -1,1 +1,8 @@
 import { createStore } from "https://cdn.skypack.dev/redux";
+import profileReducer from "./profileReducer";
+
+const store = createStore(profileReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
