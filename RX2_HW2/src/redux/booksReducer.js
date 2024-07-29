c258a0075fceb8f08f01d3dd3b36027ecac9f03d
7vTr0x7 <7vTrox7@gmail.com>
2024-07-29 17:42:18 +0530

    RX2_HW2 - redux - bookReducer

--- RX2_HW2/src/redux/booksReducer.js
+++ RX2_HW2/src/redux/booksReducer.js
@@ -1,19 +1,21 @@
+import { ADD_BOOK, REMOVE_BOOK, TOTAL_BOOKS } from "./action";
+
 const initialState = { books: [], totalBooks: 0 };
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_BOOK":
+    case ADD_BOOK:
       return {
         ...state,
         books: [...state.books, action.payload],
       };
-    case "REMOVE_BOOK":
+    case REMOVE_BOOK:
       return {
         ...state,
         books: state.books.filter((book) => book.ISBN != action.payload),
       };
 
-    case "TOTAL_BOOKS":
+    case TOTAL_BOOKS:
       return { ...state, totalBooks: state.books.length };
 
     default:
