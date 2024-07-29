4433fe079b46781b16daee821b036ab1cffa3782
7vTr0x7 <7vTrox7@gmail.com>
2024-07-29 18:10:00 +0530

    RX2_HW2 - components - Library

--- RX2_HW2/src/components/Library.jsx
+++ RX2_HW2/src/components/Library.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { REMOVE_BOOK } from "../redux/action";
+import { useDispatch } from "react-redux";
 
 const Library = () => {
   const totalBooks = useSelector((state) => state.totalBooks);
+  const books = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+
+  const removeBookHandler = (isbn) => {
+    dispatch({ type: REMOVE_BOOK, payload: isbn });
+  };
 
   return (
     <div>
       <h2>Library Summary</h2>
       <p>Total Books: {totalBooks}</p>
+      <ul>
+        {books.length > 0 &&
+          books.map((book) => (
+            <li key={book.ISBN}>
+              {`${book.title} by ${book.author} (ISBN: ${book.ISBN})`}{" "}
+              <button onClick={() => removeBookHandler(book.ISBN)}>
+                Remove
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
