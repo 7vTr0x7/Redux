e90aa7c4b4ef7eb7c4274c8d8797de8c79faa07f
7vTr0x7 <7vTrox7@gmail.com>
2024-07-29 11:15:21 +0530

    RX1_HW2 - changes

--- RX1_HW2/script.js
+++ RX1_HW2/script.js
@@ -1,31 +1,13 @@
 import { createStore } from "https://cdn.skypack.dev/redux";
+
 import profileReducer from "./profileReducer.js";
+import {
+  ADD_PROFILE,
+  CALCULATE_AVERAGE_AGE,
+  REMOVE_PROFILE,
+} from "./action.js";
 
 const store = createStore(profileReducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-  updateAverageAge();
-});
-
-const profiles = [
-  { id: 1, name: "Alice", age: 25 },
-  { id: 2, name: "Bob", age: 30 },
-  { id: 3, name: "Charlie", age: 35 },
-];
-
-const profilesList = document.querySelector("#profilesList");
-
-const renderProfiles = (profiles) => {
-  profilesList.innerHTML = profiles
-    .map(
-      (profile) =>
-        `<li key={${profile.id}}>${profile.id}. ${profile.name} ( ${profile.age} years old )</li>`
-    )
-    .join("");
-};
-
-renderProfiles(profiles);
 
 const avgAge = document.querySelector("#avgAge");
 
@@ -34,3 +16,40 @@ const updateAverageAge = () => {
   avgAge.textContent = `Average Age: ${state.averageAge}`;
 };
 updateAverageAge();
+
+store.subscribe(() => {
+  console.log(store.getState());
+  updateAverageAge();
+});
+
+const addProfile = (profile) => {
+  store.dispatch({
+    type: ADD_PROFILE,
+    payload: profile,
+  });
+};
+
+const calculateAvgAge = () => {
+  store.dispatch({
+    type: CALCULATE_AVERAGE_AGE,
+  });
+};
+
+const removeProfile = (id) => {
+  store.dispatch({
+    type: REMOVE_PROFILE,
+    payload: id,
+  });
+};
+
+const profilesList = document.querySelector("#profilesList");
+
+const renderProfiles = () => {
+  const state = store.getState();
+  profilesList.innerHTML = state.profiles
+    .map(
+      (profile) =>
+        `<li key={${profile.id}}>${profile.id}. ${profile.name} ( ${profile.age} years old )</li>`
+    )
+    .join("");
+};
