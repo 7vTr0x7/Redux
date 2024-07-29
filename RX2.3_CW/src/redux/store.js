d263b2774cc814ce6ee9f35e884321036e5a3124
7vTr0x7 <7vTrox7@gmail.com>
2024-07-29 17:07:17 +0530

    RX2.3_CW - applyMiddleware

--- RX2.3_CW/src/redux/store.js
+++ RX2.3_CW/src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import counterReducer from "./counterReducer";
+import loggerMiddleware from "./loggerMiddleware";
 
-const store = createStore(counterReducer);
+const store = createStore(counterReducer, applyMiddleware(loggerMiddleware));
 
 export default store;
