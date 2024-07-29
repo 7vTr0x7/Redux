08c2c101fe12eb491586687c04b6915bb9a306c1
7vTr0x7 <7vTrox7@gmail.com>
2024-07-29 17:22:44 +0530

    RX2_HW1 - applyMiddleware

--- RX2_HW1/src/redux/store.js
+++ RX2_HW1/src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import financeReducer from "./financeReducer";
+import loggerMiddleware from "./loggerMiddleware";
 
-const store = createStore(financeReducer);
+const store = createStore(financeReducer, applyMiddleware(loggerMiddleware));
 
 export default store;
