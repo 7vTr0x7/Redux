b92187b91f03006314ecdb860e04ef03ad8a616f
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 15:51:43 +0530

    RX2_HW3 - RemovedItems

--- RX2_HW3/src/pages/RemovedItems.jsx
+++ RX2_HW3/src/pages/RemovedItems.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removedItems } from "./../redux/actions";
 
 const RemovedItems = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.removedItems);
+
+  const totalItems = items.reduce((acc, curr) => acc + curr.itemQuantity, 0);
+
+  useEffect(() => {
+    dispatch(removedItems());
+  }, []);
+
+  return (
+    <div>
+      <h1>Removed Items</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.itemName} - {item.itemQuantity}
+          </li>
+        ))}
+      </ul>
+      <h2>Total Removed Items: {totalItems}</h2>
+    </div>
+  );
 };
 
 export default RemovedItems;
