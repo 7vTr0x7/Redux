88d53842a69070bc6a62f5695d120675c5e10149
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 16:39:21 +0530

    RX3.1_CW - postSlice

--- RX3.1_CW/src/App.jsx
+++ RX3.1_CW/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>My Social Media App</h1>
+      <div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default App;
