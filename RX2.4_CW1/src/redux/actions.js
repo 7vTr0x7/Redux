9da17f3cee2f3f7d3e45b241bf68ff3c4a38b99b
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 13:02:20 +0530

    RX2.4_CW1 - actions - addExpense

--- RX2.4_CW1/src/redux/actions.js
+++ RX2.4_CW1/src/redux/actions.js
@@ -2,10 +2,36 @@ export const FETCH_INCOME = "FETCH_INCOME_SUCCESS";
 export const FETCH_EXPENSES = "FETCH_EXPENSES_SUCCESS";
 export const ADD_ENTRY = "ADD_ENTRY_SUCCESS";
 
-export const addEntry = (entry) => async (dispatch) => {
+export const addIncome = (entry) => async (dispatch) => {
   try {
     const res = await fetch(
-      " https://finance-app-backend-Student-neoG.replit.app/add-income",
+      "https://finance-app-backend-Student-neoG.replit.app/add-income",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(entry),
+      }
+    );
+
+    if (!res.ok) {
+      console.log("Failed to add entry");
+    }
+
+    const data = await res.json();
+    if (data.success === true) {
+      dispatch({ type: ADD_ENTRY, payload: data.data });
+    }
+  } catch (error) {
+    console.log("Error adding entry");
+  }
+};
+
+export const addExpense = (entry) => async (dispatch) => {
+  try {
+    const res = await fetch(
+      "https://finance-app-backend-Student-neoG.replit.app/add-expense",
       {
         method: "POST",
         headers: {
