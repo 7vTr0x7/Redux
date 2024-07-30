eb271bfdd822f9e2ffb236343a9e869399842b6d
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 11:31:43 +0530

    RX2.4_CW1 - IncomeExpenseForm

--- RX2.4_CW1/src/pages/IncomeExpenseForm.jsx
+++ RX2.4_CW1/src/pages/IncomeExpenseForm.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IncomeExpenseForm = () => {
-  return <div></div>;
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [entryType, setEntryType] = useState("");
+  return (
+    <div>
+      <h1>New Entry Page</h1>
+      <form>
+        <label htmlFor="description">Description: </label>
+        <br />
+        <input
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <br />
+        <br />
+
+        <label htmlFor="amount">Amount: </label>
+        <br />
+        <input
+          type="text"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="selectEntryType">Entry Type:</label>
+        <br />
+        <select
+          id="selectEntryType"
+          value={entryType}
+          onChange={(e) => setEntryType(e.target.value)}>
+          <option value="Income">Income</option>
+          <option value="Expense">Expense</option>
+        </select>
+        <br />
+        <br />
+        <button>Add Entry</button>
+      </form>
+    </div>
+  );
 };
 
 export default IncomeExpenseForm;
