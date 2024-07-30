9d683867a4762d6dbacaa0f21d29d7386154081a
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 15:24:46 +0530

    RX2_HW3 - redux - actions - addItemToRemovedItems

--- RX2_HW3/src/redux/actions.js
+++ RX2_HW3/src/redux/actions.js
@@ -26,3 +26,28 @@ export const addItemToItems = (entry) => async (dispatch) => {
     console.log("Error Occurred While Adding Item");
   }
 };
+
+export const addItemToRemovedItems = (entry) => async (dispatch) => {
+  try {
+    const res = await fetch(
+      "https://inventory-storage-app-backend-student-neog.replit.app/remove-from-store",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(entry),
+      }
+    );
+    if (!res.ok) {
+      console.log("Failed to add");
+    }
+    const data = await res.json();
+    if (data.success === true) {
+      console.log(data);
+      dispatch({ type: ADD_ITEM, payload: data.data });
+    }
+  } catch (error) {
+    console.log("Error Occurred While Adding Item");
+  }
+};
