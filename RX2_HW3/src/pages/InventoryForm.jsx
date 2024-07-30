849522d00f5d34b70d1bb55881c8158980eba500
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 14:59:36 +0530

    RX2_HW3 - InventoryForm

--- RX2_HW3/src/pages/InventoryForm.jsx
+++ RX2_HW3/src/pages/InventoryForm.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InventoryForm = () => {
-  return <div></div>;
+  const [itemName, setItemName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [entryType, setEntryType] = useState("Add to storage");
+
+  return (
+    <div>
+      <h1>Inventory Admin App</h1>
+      <form>
+        <label htmlFor="itemName">Item Name: </label>
+        <br />
+        <input
+          type="text"
+          id="itemName"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="quantity">Item Quantity: </label>
+        <br />
+        <input
+          type="text"
+          id="quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="entryType">Entry Type: </label>
+        <br />
+        <select id="entryType" onChange={(e) => setEntryType(e.target.value)}>
+          <option value="Add">Add to storage</option>
+          <option value="Remove">Remove from storage</option>
+        </select>
+        <br />
+        <br />
+        <button>Add Item Data</button>
+      </form>
+    </div>
+  );
 };
 
 export default InventoryForm;
