74ee7805eccefe20a68daff994b951b2d044c11b
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 15:46:57 +0530

    RX2_HW3 - Inventory

--- RX2_HW3/src/pages/Inventory.jsx
+++ RX2_HW3/src/pages/Inventory.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { storageItems } from "../redux/actions";
 
 const Inventory = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.items);
+
+  useEffect(() => {
+    dispatch(storageItems());
+  }, []);
+
+  return (
+    <div>
+      <h1>Inventory Items</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.itemName} - {item.itemQuantity}
+          </li>
+        ))}
+      </ul>
+      <h2>Total Inventory Items:</h2>
+    </div>
+  );
 };
 
 export default Inventory;
