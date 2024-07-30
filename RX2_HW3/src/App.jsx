8d19b744ee429749db0bea0985ffb5509d25836a
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 14:50:50 +0530

    RX2_HW3 - routes

--- RX2_HW3/src/App.jsx
+++ RX2_HW3/src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import InventoryForm from "./pages/InventoryForm";
+import Inventory from "./pages/Inventory";
+import RemovedItems from "./pages/RemovedItems";
+import RemainingItems from "./pages/RemainingItems";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/inventory">Inventory</Link>
+              </li>
+              <li>
+                <Link to="/removed">Removed Items</Link>
+              </li>
+              <li>
+                <Link to="/remaining">Remaining Items</Link>
+              </li>
+              <li>
+                <Link to="/">Add New Item</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<InventoryForm />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/removed" element={<RemovedItems />} />
+          <Route path="/remaining" element={<RemainingItems />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
