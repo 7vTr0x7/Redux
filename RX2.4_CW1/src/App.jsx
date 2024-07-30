5c98b53d68740551e03e2252ec6a10f05c0e91da
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 11:18:17 +0530

    RX2.4_CW1 - Routes

--- RX2.4_CW1/src/App.jsx
+++ RX2.4_CW1/src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Income from "./pages/Income";
+import Expense from "./pages/Expense";
+import Savings from "./pages/Savings";
+import IncomeExpenseForm from "./pages/IncomeExpenseForm";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/income">Income</Link>
+          </li>
+          <li>
+            <Link to="/expense">Expense</Link>
+          </li>
+          <li>
+            <Link to="/savings">Savings</Link>
+          </li>
+          <li>
+            <Link to="/">New Entries</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/savings" element={<Savings />} />
+          <Route path="/" element={<IncomeExpenseForm />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
