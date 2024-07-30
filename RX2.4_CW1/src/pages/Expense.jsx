654f418631b08b99e1b19f5e195b33997f6f3bf7
7vTr0x7 <7vTrox7@gmail.com>
2024-07-30 12:58:24 +0530

    RX2.4_CW1 - Expense

--- RX2.4_CW1/src/pages/Expense.jsx
+++ RX2.4_CW1/src/pages/Expense.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExpenses } from "../redux/actions";
 
-const Expense = () => {
-  return <div></div>;
+const Expenses = () => {
+  const dispatch = useDispatch();
+
+  const expenses = useSelector((state) => state.expenses);
+
+  const totalExpense = expenses.reduce((acc, curr) => acc + curr.amount, 0);
+
+  useEffect(() => {
+    dispatch(fetchExpenses());
+  }, []);
+
+  return (
+    <div>
+      <h1>Expenses Page</h1>
+      <ul>
+        {expenses.map((exp, index) => (
+          <li key={index}>
+            {exp.description} - ${exp.amount}
+          </li>
+        ))}
+      </ul>
+      <h2>Expenses Total: ${totalExpense}</h2>
+    </div>
+  );
 };
 
-export default Expense;
+export default Expenses;
