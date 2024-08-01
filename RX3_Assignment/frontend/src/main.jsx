425a7237ecd6ead4853b753bd5f88a6f00704333
7vTr0x7 <7vTrox7@gmail.com>
2024-08-01 14:33:15 +0530

    RX3_Assignment - frontend - bootstrap

--- RX3_Assignment/frontend/src/main.jsx
+++ RX3_Assignment/frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
