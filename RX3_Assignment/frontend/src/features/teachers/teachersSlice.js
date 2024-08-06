a83b45dbfddab25edc6ef01b637046e5c297bce9
7vTr0x7 <7vTrox7@gmail.com>
2024-08-06 17:35:13 +0530

    RX3_Assignment - frontend - features - teachers - teachersSlice

---
+++ RX3_Assignment/frontend/src/features/teachers/teachersSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const teachersSlice = createSlice({
+  name: "teachers",
+  initialState: {
+    teachers: [
+      {
+        _id: "1",
+        name: "mr. shinde",
+        age: 55,
+        gender: "Male",
+        subject: "Python",
+      },
+    ],
+  },
+  reducers: {
+    addTeacher: (state, action) => {
+      return {
+        ...state,
+        teachers: [...state.teachers, action.payload],
+      };
+    },
+    deleteTeacher: (state, action) => {
+      return {
+        ...state,
+        teachers: state.teachers.filter(
+          (teacher) => teacher._id !== action.payload
+        ),
+      };
+    },
+    updateTeacher: (state, action) => {
+      const teacherIndex = state.teachers.findIndex(
+        (teacher) => teacher._id === action.payload.id
+      );
+      state.teachers[teacherIndex] = action.payload.teacher;
+    },
+  },
+});
+
+export const { addTeacher, deleteTeacher, updateTeacher } =
+  teachersSlice.actions;
